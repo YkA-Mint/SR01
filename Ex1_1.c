53413d1f52ca6cca60bc100de715f1c331a7329a
Libra_XIAO <75241623+YkA-Mint@users.noreply.github.com>
2023-10-18 21:32:59 +0200

    Ex1_1.c

--- Ex1_1.c
+++ Ex1_1.c
@@ -4,13 +4,18 @@ int main()
 {
     int A=20,B =5;
     int C=!-- A /++!B;
-    /*
-    Õâ¸ö´úÂëÓÉÓÚ++Ö»ÄÜÓÃÓÚ±äÁ¿£¬¶ø£¡BÊÇÒ»¸öÓÒÖµ£¬µ¼ÖÂÎŞ·¨±àÒëÍ¨¹ı
-    °´ÕÕÀíÂÛ·ÖÎöµÄ»°£¬½á¹ûÓ¦¸ÃÊÇ(!(--A))/(++(!B))
+     /*
+    Cette partie du code gÃ©nÃ¨re une erreur de compilation en raison de problÃ¨mes liÃ©s aux valeurs de gauche (lvalue) et de droite (rvalue).
+    !B est un rvalue(l'address n'existe pas), mais '++' doit combiner avec un variable (lvalue)
+    (!(--A))/(++(!B)) cette expression est ma conjecture , donc A=19 B=? ,C=0
+    Je pense que '!' ne peux pas changer la valeur de B et '++', theoriquement, incrementer '!B',pas'B'
+    Peut-etre B = 5(ne changer pas)
 
+    J'aimerais savoir ce que tu en penses. :-ï¼‰
 
 
     */
+
     printf (" A=% d B=% d c=% d \ n", A,B, C);
     return 0;
 }
